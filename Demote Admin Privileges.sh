a1d10c1597b07061c170410e151232d9576dde1f
Peter Lewis <peter@peterlew.is>
2022-11-01 10:42:12 +0000

    Update Demote Admin Privileges.sh

--- Demote Admin Privileges.sh
+++ Demote Admin Privileges.sh
@@ -47,10 +47,9 @@ admin_to_exclude="${8}"
 # Leave blank to use default location of /Applications/IBM Notifier.app
 ibm_notifier_path="${9}"
 
-
-# Enter the binary name used in IBM Notifier if it is not standard.
+# Enter the binary for IBM Notifier if it is not standard.
 # Leave blank to use default name of 'IBM Notifier'
-ibm_notifier_binary_name="${10}"
+ibm_notifier_binary="${10}"
 
 # Check for IBM Notifier path in parameter 9. If blank, set default path
 if [ ! "$ibm_notifier_path" ]; then
@@ -58,8 +57,8 @@ if [ ! "$ibm_notifier_path" ]; then
 fi
 
 # Check for IBM Notifier binary name in parameter 10. If blank, set default path
-if [ ! "$ibm_notifier_binary_name" ]; then
-	ibm_notifier_binary_name="IBM Notifier"
+if [ ! "$ibm_notifier_binary" ]; then
+	ibm_notifier_binary="IBM Notifier"
 fi
 
 
@@ -149,7 +148,7 @@ prompt_with_ibmNotifier () {
 	
 	# Prompt the user
 	prompt_user() {
-		button=$( "${ibm_notifier_path}/Contents/MacOS/${ibm_notifier_binary_name}" \
+		button=$( "${ibm_notifier_path}/Contents/MacOS/${ibm_notifier_binary}" \
 		-type "popup" \
 		-bar_title "Privileges Reminder" \
 		-subtitle "You are currently an administrator on this device.
