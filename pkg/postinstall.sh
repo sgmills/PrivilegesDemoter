de5f3cc487bb8b4197fb8ad9670fe7ccdb6d7745
Sam Mills <s.mills131@gmail.com>
2023-02-22 13:47:52 -0500

    launchd updates

--- pkg/postinstall.sh
+++ pkg/postinstall.sh
@@ -2,6 +2,3 @@
 
 # Load launchDaemon to check for admin every 5 minutes
 launchctl bootstrap system /Library/LaunchDaemons/blog.mostlymac.privileges.check.plist
-
-# Load launchDaemon to confirm and optionally demote user if time limit is reached
-launchctl bootstrap system /Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist
