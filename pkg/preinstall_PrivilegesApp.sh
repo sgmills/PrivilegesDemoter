c24dad8c699ab3af92ffcf789a4e7faa5f65b245
Sam Mills <s.mills131@gmail.com>
2023-02-22 12:49:16 -0800

    update installer scripts

--- pkg/preinstall_PrivilegesApp.sh
+++ pkg/preinstall_PrivilegesApp.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
-# Unload launchDaemon to check for admin every 5 minutes if needed
-if launchctl list | grep -q blog.mostlymac.privileges.check; then
-	launchctl bootout system /Library/LaunchDaemons/blog.mostlymac.privileges.check.plist
-fi
-
-# Unload launchDaemon to confirm and optionally demote user if time limit is reached if needed
-if launchctl list | grep -q blog.mostlymac.privileges.demote; then
-	launchctl bootout system /Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist
-fi
-
-# Remove demote file if needed
-if [ -f /tmp/privilegesDemote ]; then
-	rm /tmp/privilegesDemote
-fi
-
-# Remove check file if needed
-if [ -f /tmp/privilegesCheck ]; then
-	rm /tmp/privilegesCheck
-fi
-
-# Remove log file if needed
-if [ -f /var/log/privileges.log ]; then
-	rm /var/log/privileges.log
-fi
-
-# Remove cached demotion script if needed
-if [ -d "/Library/Application Support/JAMF/Offline Policies/privilegesDemote" ]; then
-	rm -rf "/Library/Application Support/JAMF/Offline Policies/privilegesDemote"
-fi
-
 # Determine working directory
 install_dir=$( dirname "$0" )
 
 # Install Privileges.app in the working directory
 /usr/sbin/installer -dumplog -verbose -pkg "$install_dir"/"Privileges-1.5.3.pkg" -target "$3"
+
+# Unload launchDaemon to check for admin every 5 minutes
+launchctl bootout system /Library/LaunchDaemons/blog.mostlymac.privileges.check.plist 2>/dev/null
+
+# Note: All of the following are depricated as of v3. Removing unnecessary bits
+# Remove launchDaemon to confirm and optionally demote user if time limit is reached
+launchctl bootout system /Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist 2>/dev/null 
+launchctl remove /Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist 2>/dev/null
+rm -f Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist 2>/dev/null
+
+# Remove demote file
+rm /tmp/privilegesDemote 2>/dev/null
+
+# Remove check file
+rm /tmp/privilegesCheck 2>/dev/null
+
+# Remove log file
+rm /var/log/privileges.log 2>/dev/null
+
+# Remove cached demotion script
+rm -rf "/Library/Application Support/JAMF/Offline Policies/privilegesDemote" 2>/dev/null
