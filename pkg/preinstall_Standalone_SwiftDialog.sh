ab46e8153045e71b3a6e465391717f545c3cd509
Sam Mills <s.mills131@gmail.com>
2023-03-22 13:27:03 -0400

    preinstall files

--- pkg/preinstall_Standalone_SwiftDialog.sh
+++ pkg/preinstall_Standalone_SwiftDialog.sh
@@ -3,26 +3,26 @@
 # Determine working directory
 install_dir=$( dirname "$0" )
 
-# Install Privileges.app in the working directory
-/usr/sbin/installer -dumplog -verbose -pkg "$install_dir"/"Privileges-1.5.3.pkg" -target "$3"
+# Install Swift Dialog in the working directory
+/usr/sbin/installer -dumplog -verbose -pkg "$install_dir"/"dialog-2.1.0-4148.pkg" -target "$3"
 
 # Unload launchDaemon to check for admin every 5 minutes
 launchctl bootout system /Library/LaunchDaemons/blog.mostlymac.privileges.check.plist 2>/dev/null
 
+# Remove old script/folder
+rm -rf /usr/local/mostlymac 2>/dev/null
+
+# Remove check file
+rm /tmp/privilegesCheck 2>/dev/null
+
 # Note: All of the following are depricated as of v3. Removing unnecessary bits
 # Remove launchDaemon to confirm and optionally demote user if time limit is reached
-launchctl bootout system /Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist 2>/dev/null 
+launchctl bootout system /Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist 2>/dev/null
 launchctl remove /Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist 2>/dev/null
 rm -f Library/LaunchDaemons/blog.mostlymac.privileges.demote.plist 2>/dev/null
 
 # Remove demote file
 rm /tmp/privilegesDemote 2>/dev/null
-
-# Remove check file
-rm /tmp/privilegesCheck 2>/dev/null
-
-# Remove log file
-rm /var/log/privileges.log 2>/dev/null
 
 # Remove cached demotion script
 rm -rf "/Library/Application Support/JAMF/Offline Policies/privilegesDemote" 2>/dev/null
